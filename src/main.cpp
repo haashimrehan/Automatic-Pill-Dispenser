304ca726937def80036fe9bcd2148af8abbdfe7b
Haashim Rehan <haashimr@hotmail.com>
2021-03-07 11:54:18 -0500

    servo control

--- src/main.cpp
+++ src/main.cpp
@@ -1,213 +1,140 @@
-#include <ESP8266WiFi.h>
-#include <FirebaseESP8266.h>
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#include <ESP32Servo.h>
+
 #include <Credentials.h>
 
-/* 1. Define the WiFi credentials */
-#include <Arduino.h>
-
-/* 3. Define the Firebase Data object */
+//2. Define FirebaseESP8266 data object for data sending and receiving
 FirebaseData fbdo;
 
-/* 4, Define the FirebaseAuth data for authentication data */
-FirebaseAuth auth;
+Servo myservo; 
+int servoPin = 18;      // GPIO pin used to connect the servo control (digital out)
+// Possible ADC pins on the ESP32: 0,2,4,12-15,32-39; 34-39 are recommended for analog input
+int ADC_Max = 4096;   
 
-/* Define the FirebaseConfig data for config data */
-FirebaseConfig config;
-
-/* The function to print the operating results */
-void printResult(FirebaseData &data);
-
-String path = "/Test";
-unsigned long dataMillis = 0;
-int count = 0;
-
+void dispense() {
+myservo.write(10);                  // set the servo position according to the scaled value
+  delay(2000); 
+  myservo.write(180);                  // set the servo position according to the scaled value
+  delay(2000);
+}
 void setup()
 {
-    Serial.begin(115200);
+	// Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+    myservo.setPeriodHertz(50);// Standard 50hz servo
+  myservo.attach(servoPin, 500, 2400); 
 
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(300);
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  //4. Enable auto reconnect the WiFi when connection lost
+  Firebase.reconnectWiFi(true);
+
+  //5. Try to set int data to Firebase
+  //The set function returns bool for the status of operation
+  //fbdo requires for sending the data
+  if(Firebase.setInt(fbdo, "/LED_Status", 1))
+  {
+    //Success
+     Serial.println("Set int data success");
+
+  }else{
+    //Failed?, get the error reason from fbdo
+
+    Serial.print("Error in setInt, ");
+    Serial.println(fbdo.errorReason());
+  }
+
+//int[] schedule = new int[7];
+ int schedule[7];
+  //6. Try to get int data from Firebase
+  //The get function returns bool for the status of operation
+  //fbdo requires for receiving the data
+  if(Firebase.getString(fbdo, "/Pills/pill1/Schedule"))
+  {
+    //Success
+    Serial.print("Get int data success, int = ");
+  
+    String sch = fbdo.stringData();
+     for (int i = 0; i < 12; i++){
+         String t =sch.substring(i-1,i); 
+         if (t == ",") continue;
+        Serial.println(t);
+        schedule[i] = t.toInt();
+   // Serial.println(schedule[i]);
+     }
+     //schedule = sch.split(",");
+     //char* command = strtok(sch, ",");
+     //String str = "123 456 789";
+
+    Serial.println(fbdo.stringData());
+
+  }else{
+    //Failed?, get the error reason from fbdo
+
+    Serial.print("Error in getInt, ");
+    Serial.println(fbdo.errorReason());
+  }
+
+  /*
+  In case where you want to set other data types i.e. bool, float, double and String, you can use setBool, setFloat, setDouble and setString.
+  If you want to get data which you known its type at specific node, use getInt, getBool, getFloat, getDouble, getString.
+  If you don't know the data type at specific node, use get and check its type.
+  The following shows how to get the variant data
+  */
+
+ if(Firebase.get(fbdo, "/"))
+  {
+    //Success
+    Serial.print("Get variant data success, type = ");
+    Serial.println(fbdo.dataType());
+
+    if(fbdo.dataType() == "int"){
+      Serial.print("data = ");
+      Serial.println(fbdo.intData());
+    }else if(fbdo.dataType() == "bool"){
+      if(fbdo.boolData())
+        Serial.println("data = true");
+      else
+        Serial.println("data = false");
     }
-    Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
-    Serial.println();
 
-    /* Assign the certificate file (optional) */
-    //config.cert.file = "/cert.cer";
-    //config.cert.file_storage = StorageType::FLASH;
+  }else{
+    //Failed?, get the error reason from fbdo
 
-    /* Assign the project host and database secret(required) */
-    config.host = FIREBASE_HOST;
-    config.signer.tokens.legacy_token = FIREBASE_AUTH;
+    Serial.print("Error in get, ");
+    Serial.println(fbdo.errorReason());
+  }
 
+  /*
+  If you want to get the data in realtime instead of using get, see the stream examples.
+  If you want to work with JSON, see the FirebaseJson, jsonObject and jsonArray examples.
+  If you have questions or found the bugs, feel free to open the issue here https://github.com/mobizt/Firebase-ESP32
+  */
+dispense();
+dispense();
 
-    Firebase.reconnectWiFi(true);
-
-    /* Initialize the library with the Firebase authen and config */
-    Firebase.begin(&config, &auth);
 }
+
 
 void loop()
-{
-    if (millis() - dataMillis > 5000)
-    {
-        dataMillis = millis();
+{//dispense();
 
-        Serial.println("------------------------------------");
-        Serial.println("Set int test...");
-
-        if (Firebase.set(fbdo, path + "/int", count++))
-        {
-            Serial.println("PASSED");
-            Serial.println("PATH: " + fbdo.dataPath());
-            Serial.println("TYPE: " + fbdo.dataType());
-            Serial.println("ETag: " + fbdo.ETag());
-            Serial.print("VALUE: ");
-            printResult(fbdo);
-            Serial.println("------------------------------------");
-            Serial.println();
-        }
-        else
-        {
-            Serial.println("FAILED");
-            Serial.println("REASON: " + fbdo.errorReason());
-            Serial.println("------------------------------------");
-            Serial.println();
-        }
-    }
-}
-
-void printResult(FirebaseData &data)
-{
-
-    if (data.dataType() == "int")
-        Serial.println(data.intData());
-    else if (data.dataType() == "float")
-        Serial.println(data.floatData(), 5);
-    else if (data.dataType() == "double")
-        printf("%.9lf\n", data.doubleData());
-    else if (data.dataType() == "boolean")
-        Serial.println(data.boolData() == 1 ? "true" : "false");
-    else if (data.dataType() == "string")
-        Serial.println(data.stringData());
-    else if (data.dataType() == "json")
-    {
-        Serial.println();
-        FirebaseJson &json = data.jsonObject();
-        //Print all object data
-        Serial.println("Pretty printed JSON data:");
-        String jsonStr;
-        json.toString(jsonStr, true);
-        Serial.println(jsonStr);
-        Serial.println();
-        Serial.println("Iterate JSON data:");
-        Serial.println();
-        size_t len = json.iteratorBegin();
-        String key, value = "";
-        int type = 0;
-        for (size_t i = 0; i < len; i++)
-        {
-            json.iteratorGet(i, type, key, value);
-            Serial.print(i);
-            Serial.print(", ");
-            Serial.print("Type: ");
-            Serial.print(type == FirebaseJson::JSON_OBJECT ? "object" : "array");
-            if (type == FirebaseJson::JSON_OBJECT)
-            {
-                Serial.print(", Key: ");
-                Serial.print(key);
-            }
-            Serial.print(", Value: ");
-            Serial.println(value);
-        }
-        json.iteratorEnd();
-    }
-    else if (data.dataType() == "array")
-    {
-        Serial.println();
-        //get array data from FirebaseData using FirebaseJsonArray object
-        FirebaseJsonArray &arr = data.jsonArray();
-        //Print all array values
-        Serial.println("Pretty printed Array:");
-        String arrStr;
-        arr.toString(arrStr, true);
-        Serial.println(arrStr);
-        Serial.println();
-        Serial.println("Iterate array values:");
-        Serial.println();
-        for (size_t i = 0; i < arr.size(); i++)
-        {
-            Serial.print(i);
-            Serial.print(", Value: ");
-
-            FirebaseJsonData &jsonData = data.jsonData();
-            //Get the result data from FirebaseJsonArray object
-            arr.get(jsonData, i);
-            if (jsonData.typeNum == FirebaseJson::JSON_BOOL)
-                Serial.println(jsonData.boolValue ? "true" : "false");
-            else if (jsonData.typeNum == FirebaseJson::JSON_INT)
-                Serial.println(jsonData.intValue);
-            else if (jsonData.typeNum == FirebaseJson::JSON_FLOAT)
-                Serial.println(jsonData.floatValue);
-            else if (jsonData.typeNum == FirebaseJson::JSON_DOUBLE)
-                printf("%.9lf\n", jsonData.doubleValue);
-            else if (jsonData.typeNum == FirebaseJson::JSON_STRING ||
-                     jsonData.typeNum == FirebaseJson::JSON_NULL ||
-                     jsonData.typeNum == FirebaseJson::JSON_OBJECT ||
-                     jsonData.typeNum == FirebaseJson::JSON_ARRAY)
-                Serial.println(jsonData.stringValue);
-        }
-    }
-    else if (data.dataType() == "blob")
-    {
-
-        Serial.println();
-
-        for (size_t i = 0; i < data.blobData().size(); i++)
-        {
-            if (i > 0 && i % 16 == 0)
-                Serial.println();
-
-            if (i < 16)
-                Serial.print("0");
-
-            Serial.print(data.blobData()[i], HEX);
-            Serial.print(" ");
-        }
-        Serial.println();
-    }
-    else if (data.dataType() == "file")
-    {
-
-        Serial.println();
-
-        File file = data.fileStream();
-        int i = 0;
-
-        while (file.available())
-        {
-            if (i > 0 && i % 16 == 0)
-                Serial.println();
-
-            int v = file.read();
-
-            if (v < 16)
-                Serial.print("0");
-
-            Serial.print(v, HEX);
-            Serial.print(" ");
-            i++;
-        }
-        Serial.println();
-        file.close();
-    }
-    else
-    {
-        Serial.println(data.payload());
-    }
 }
